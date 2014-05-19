3d8e549e568aafb8193976c24d2403aacaf89a4f
Joe <joemaag.me@gmail.com>
2014-05-19 15:34:10 -0400

    Basic main menu UI laid out

--- MobilePoet/MenuViewController.m
+++ MobilePoet/MenuViewController.m
@@ -9,21 +9,56 @@
 #import "MenuViewController.h"
 
 @interface MenuViewController ()
-
+@property(nonatomic, strong) UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UIButton *beginButton;
+@property (weak, nonatomic) IBOutlet UIButton *trainingModeButton;
+@property (weak, nonatomic) IBOutlet UIButton *helpButton;
+@property (weak, nonatomic) IBOutlet UIButton *aboutButton;
+@property (weak, nonatomic) IBOutlet UIButton *signInButton;
 @end
 
 @implementation MenuViewController
 
+#pragma mark - Setup
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    self.titleLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 300, 60)];
+    self.titleLabel.center = CGPointMake(self.view.center.x, 80);
+    self.titleLabel.backgroundColor = [UIColor clearColor];
+    self.titleLabel.font = [UIFont fontWithName:@"Avenir-Light" size:34.0];
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
+    self.titleLabel.text = @"Poet Mobile";
+    [self.view addSubview:self.titleLabel];
+    
 }
 
-- (void)didReceiveMemoryWarning
+-(void)viewDidLayoutSubviews
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewDidLayoutSubviews];
+    
+    //Setup for intro animation
+    
+    
+}
+
+#pragma mark - Button actions
+
+- (IBAction)beginButtonPressed:(id)sender {
+}
+
+- (IBAction)trainingModeButtonPressed:(id)sender {
+}
+
+- (IBAction)helpButtonPressed:(id)sender {
+}
+
+- (IBAction)aboutButtonPressed:(id)sender {
+}
+
+- (IBAction)signInButtonPressed:(id)sender {
 }
 
 @end
