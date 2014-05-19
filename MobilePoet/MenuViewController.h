909b4cefede48654f89064fc7e778cdc0aea07dc
Joe <joemaag.me@gmail.com>
2014-05-19 14:15:35 -0400

    Xcode project created

---
+++ MobilePoet/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MobilePoet
+//
+//  Created by Joseph Maag on 5/19/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
---
+++ MobilePoet/MenuViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MobilePoet
+//
+//  Created by Joseph Maag on 5/19/14.
+//
+//
+
+#import "MenuViewController.h"
+
+@interface MenuViewController ()
+
+@end
+
+@implementation MenuViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
